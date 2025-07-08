2ec927535060392a521ed2e17a7493ba08fb01a0
Jodie Glantzlen <jodie.glantzlen@gmail.com>
2025-07-08 17:46:54 +0200

    Allow CORS policy for frontend

--- src/BloomBridge.Api/Program.cs
+++ src/BloomBridge.Api/Program.cs
@@ -10,11 +10,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=BloomBridge.db"));
 builder.Services.AddScoped<ITherapistMatcherService, TherapistMatcherService>();
 
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhostFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:8081")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
+
+app.UseCors("AllowLocalhostFrontend");
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
